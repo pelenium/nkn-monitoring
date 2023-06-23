99d569566c80a53187ca47e4037382b818a22b7d
PELENIUM <i.d.chernyshov@yandex.ru>
2023-06-23 22:31:55 +0300

    .

--- nkn-scripts/testing410.sh
+++ nkn-scripts/testing410.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 apt update -y
 apt purge needrestart -y
 apt-mark hold linux-image-generic linux-headers-generic openssh-server snapd
@@ -14,16 +12,23 @@ keys="https://nknrus.ru/g410.tar"
 useradd -m -p "pass" -s /bin/bash "$username" > /dev/null 2>&1
 usermod -a -G sudo "$username" > /dev/null 2>&1
 
-printf "Downloading NKN Commercial........................... "
+printf "Downloading........................................... "
 cd /home/$username > /dev/null 2>&1
 wget --quiet --continue --show-progress https://commercial.nkn.org/downloads/nkn-commercial/linux-amd64.zip > /dev/null 2>&1
+printf "DONE!\n"
+
+printf "Installing............................................ "
 unzip linux-amd64.zip > /dev/null 2>&1
 mv linux-amd64 nkn-commercial > /dev/null 2>&1
 chown -c $username:$username nkn-commercial/ > /dev/null 2>&1
 /home/$username/nkn-commercial/nkn-commercial -b $benaddress -d /home/$username/nkn-commercial/ -u $username install > /dev/null 2>&1
 printf "DONE!\n"
+printf "sleep 180"
+
+sleep 180
 
 DIR="/home/$username/nkn-commercial/services/nkn-node/"
+
 systemctl stop nkn-commercial.service > /dev/null 2>&1
 sleep 20
 cd $DIR > /dev/null 2>&1
@@ -31,20 +36,9 @@ rm wallet.json > /dev/null 2>&1
 rm wallet.pswd > /dev/null 2>&1
 rm config.json > /dev/null 2>&1
 rm -Rf ChainDB > /dev/null 2>&1
-
-ip=$(ifconfig | grep 'inet addr:' | grep -v '127.0.0.1' | awk '{print $2}' | cut -f2 -d':')
-export NODE_IP="$ip"
-
-json="{\"ip\": \"$ip\"}"
-echo "$json"
-
 wget -O - "$keys" -q --show-progress | tar -xf -
 wget -O - "$config" -q --show-progress | tar -xf -
 chown -R $username:$username wallet.* > /dev/null 2>&1
 chown -R $username:$username config.* > /dev/null 2>&1
-
-curl -X POST -H "Content-Type: application/json" -d json http://146.190.65.152:9999/
-
-printf "Starting NKN node.................................... "
+printf "Downloading.......................................... DONE!\n"
 systemctl start nkn-commercial.service > /dev/null 2>&1
-printf "DONE!\n"
