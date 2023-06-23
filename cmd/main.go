1848cfc4f6705de66b8e4d98863ff8924a7d8c63
PELENIUM <i.d.chernyshov@yandex.ru>
2023-06-23 13:56:27 +0300

    changed mistakes with db

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,34 @@
 package main
 
 import (
+	"database/sql"
 	"monitoring/internal/handlers"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
 	router := gin.Default()
 
+	db, err := sql.Open("sqlite3", "./../db/nodes.sqlite")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	create := "CREATE TABLE nodes-ip (ip TEXT PRIMARY KEY)"
+	_, err = db.Exec(create)
+
+	if err != nil {
+		panic(err)
+	}
+
 	router.LoadHTMLGlob("./../internal/html/*.html")
 
 	router.GET("/", handlers.MainPageGET)
 
-	router.POST("/", handlers.MainPagePOST)
+	router.POST("/", handlers.MainPagePOST(db))
 
 	router.Run(":9999")
 }
