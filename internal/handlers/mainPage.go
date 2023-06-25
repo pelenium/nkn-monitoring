e1816ed2569353646f0be3930156b022780f397b
PELENIUM <i.d.chernyshov@yandex.ru>
2023-06-25 14:19:40 +0300

    .

--- internal/handlers/mainPage.go
+++ internal/handlers/mainPage.go
@@ -42,32 +42,36 @@ func NodeIpPOST(db *sql.DB) gin.HandlerFunc {
 				if err != nil {
 					panic(err)
 				}
-				rows, err := db.Query("SELECT * FROM nodes_ip")
-				if err != nil {
-					panic(err)
-				}
-				defer rows.Close()
-				cols, err := rows.Columns()
-				if err != nil {
-					panic(err)
-				}
-				all_ips := make([]interface{}, len(cols))
-				for i := range cols {
-					all_ips[i] = new(interface{})
-				}
-				for rows.Next() {
-					err = rows.Scan(all_ips...)
-					if err != nil {
-						panic(err)
-					}
-
-					for i, column := range cols {
-						val := *(all_ips[i].(*interface{}))
-						fmt.Println(column, val)
-					}
-				}
-				fmt.Println(ip)
 			}
+
+			rows, err := db.Query("SELECT * FROM nodes_ip")
+			if err != nil {
+				panic(err)
+			}
+
+			defer rows.Close()
+			cols, err := rows.Columns()
+			if err != nil {
+				panic(err)
+			}
+			
+			all_ips := make([]interface{}, len(cols))
+			for i := range cols {
+				all_ips[i] = new(interface{})
+			}
+
+			for rows.Next() {
+				err = rows.Scan(all_ips...)
+				if err != nil {
+					panic(err)
+				}
+
+				for i, column := range cols {
+					val := *(all_ips[i].(*interface{}))
+					fmt.Println(column, val)
+				}
+			}
+			fmt.Println(ip)
 		}
 	}
 }
