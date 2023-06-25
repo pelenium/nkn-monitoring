226d8fecfe72f47fb07d5141e6c36347230024c5
PELENIUM <i.d.chernyshov@yandex.ru>
2023-06-25 14:22:41 +0300

    .

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"monitoring/internal/handlers"
 
 	"github.com/gin-gonic/gin"
@@ -11,23 +12,21 @@ import (
 func main() {
 	router := gin.Default()
 
+	fmt.Println("started server")
+
 	db, err := sql.Open("sqlite3", "./../internal/db/nodes.sqlite")
 
 	if err != nil {
 		panic(err)
 	}
 
-	_, err = db.Exec(
-		`
-		CREATE TABLE IF NOT EXISTS nodes_ip 
-		(ip TEXT NOT NULL PRIMARY KEY,
-		latest_block_height INT,
-		node_status TEXT); DELETE FROM nodes_ip;
-		`)
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS nodes_ip (ip TEXT NOT NULL PRIMARY KEY, latest_block_height INT, node_status TEXT); DELETE FROM nodes_ip;`)
 
 	if err != nil {
 		panic(err)
 	}
+
+	fmt.Println("created table")
 
 	router.LoadHTMLGlob("./../internal/html/*.html")
 
