f46b6d91617b5eb7cfa573467db360b80a9a1dcf
PELENIUM <i.d.chernyshov@yandex.ru>
2023-06-26 22:55:12 +0300

    first api getter

--- internal/static/js/info-get.js
+++ internal/static/js/info-get.js
@@ -1,8 +1,27 @@
 fetch('/api')
     .then(response => response.json()) // Преобразуем ответ в объект JavaScript
     .then(data => {
-        for (let i in data) {
-            console.log(i);
+        for (var i = 0; i < data.length; i++) {
+            let ip = data[i];
+
+            const apiUrl = `http://${ip}:30003/`;
+            const data = { jsonrpc: '2.0', method: 'getlatestblockheight', params: {}, 'id': 1 };
+
+            fetch(apiUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+                .then(response => response.json())
+                .then(info => {
+                    let jsn = JSON.parse(info);
+                    console.log(jsn);
+                })
+                .catch(error => {
+                    console.error('Ошибка при загрузке данных:', error);
+                });
         }
     })
     .catch(error => console.error(error));
