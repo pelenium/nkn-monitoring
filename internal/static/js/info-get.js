f278c56b12d62b5637fd3b25b1cafcac8c46aff3
PELENIUM <i.d.chernyshov@yandex.ru>
2023-06-27 18:46:26 +0300

    test

--- internal/static/js/info-get.js
+++ internal/static/js/info-get.js
@@ -1,14 +1,19 @@
-fetch('/api')
-    .then(response => response.json()) // Преобразуем ответ в объект JavaScript
-    .then(data => {
-        for (var i = 0; i < data.length; i++) {
-            var ip = data[i].trim();
-            console.log(ip, "\n");
+function main() {
+    fetch('/api')
+        .then(response => response.json()) // Преобразуем ответ в объект JavaScript
+        .then(data => {
+            for (var i = 0; i < data.length; i++) {
+                var ip = data[i].trim();
+                console.log(ip, "\n");
 
-            getBlockHeight(ip);
-        }
-    })
-    .catch(error => console.error(error));
+                getBlockHeight(ip);
+                getBlockCount(ip);
+                getNodeState(ip);
+                getVersion(ip);
+            }
+        })
+        .catch(error => console.error(error));
+}
 
 function getBlockHeight(ip) {
     const url = `http://${ip}:30003`;
@@ -26,3 +31,56 @@ function getBlockHeight(ip) {
         .then(data => console.log(JSON.stringify(data)))
         .catch(error => console.error(error));
 }
+
+function getBlockCount(ip) {
+    const url = `http://${ip}:30003`;
+    const requestData = {
+        jsonrpc: "2.0",
+        method: "getblockcount",
+        params: {},
+        id: 1
+    };
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(requestData),
+    })
+        .then(response => response.json())
+        .then(data => console.log(JSON.stringify(data)))
+        .catch(error => console.error(error));
+}
+
+function getNodeState(ip) {
+    const url = `http://${ip}:30003`;
+    const requestData = {
+        jsonrpc: "2.0",
+        method: "getnodestate",
+        params: {},
+        id: 1,
+    };
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(requestData),
+    })
+        .then(response => response.json())
+        .then(data => console.log(JSON.stringify(data)))
+        .catch(error => console.error(error));
+}
+
+function getVersion(ip) {
+    const url = `http://${ip}:30003`;
+    const requestData = {
+        jsonrpc: "2.0",
+        method: "getversion",
+        params: {},
+        id: 1,
+    };
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(requestData),
+    })
+        .then(response => response.json())
+        .then(data => console.log(JSON.stringify(data)))
+        .catch(error => console.error(error));
+}
+
+setInterval(main, 10000);
