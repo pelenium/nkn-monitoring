4f621dc0e6d9e2e0ca2f368b79132c31d4c6d1e6
PELENIUM <i.d.chernyshov@yandex.ru>
2023-06-27 19:00:20 +0300

    check data

--- internal/static/js/info-get.js
+++ internal/static/js/info-get.js
@@ -1,10 +1,22 @@
+let blocksfortoday = 0;
+
 function main() {
+    const now = new Date();
+    const timezoneOffset = now.getTimezoneOffset();
+    const moscowTimezoneOffset = 180;
+    const differenceInMinutes = (moscowTimezoneOffset - timezoneOffset);
+    const differenceInMillis = differenceInMinutes * 60 * 1000;
+    const moscowNow = new Date(now.getTime() + differenceInMillis);
+
+    if (moscowNow.getHours() === 0 && moscowNow.getMinutes() === 0) {
+        saveBlockCount(blocksfortoday);
+    }
+
     fetch('/api')
         .then(response => response.json()) // Преобразуем ответ в объект JavaScript
         .then(data => {
             for (var i = 0; i < data.length; i++) {
                 var ip = data[i].trim();
-                console.log(ip, "\n");
 
                 getBlockHeight(ip);
                 getBlockCount(ip);
@@ -13,6 +25,11 @@ function main() {
             }
         })
         .catch(error => console.error(error));
+}
+
+function saveBlockCount(blocksCount) {
+    blocksfortoday = blocksCount;
+    console.log(`Saved block count for the day: ${blocksfortoday}`);
 }
 
 function getBlockHeight(ip) {
@@ -28,7 +45,9 @@ function getBlockHeight(ip) {
         body: JSON.stringify(requestData),
     })
         .then(response => response.json())
-        .then(data => console.log(JSON.stringify(data)))
+        .then(data => {
+            console.log(data.result);
+        })
         .catch(error => console.error(error));
 }
 
@@ -45,7 +64,34 @@ function getBlockCount(ip) {
         body: JSON.stringify(requestData),
     })
         .then(response => response.json())
-        .then(data => console.log(JSON.stringify(data)))
+        .then(data => {
+            console.log(JSON.stringify(data));
+            if (moscowNow.getHours() === 0 && moscowNow.getMinutes() === 0) {
+                saveBlockCount(data.result);
+            }
+        })
+        .catch(error => console.error(error));
+}
+
+function getBlockCountToday(ip) {
+    const url = `http://${ip}:30003`;
+    const requestData = {
+        jsonrpc: "2.0",
+        method: "getblockcount",
+        params: {},
+        id: 1
+    };
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(requestData),
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.result);
+            if (moscowNow.getHours() === 0 && moscowNow.getMinutes() === 0) {
+                saveBlockCount(data.result - blocksfortoday);
+            }
+        })
         .catch(error => console.error(error));
 }
 
@@ -62,7 +108,9 @@ function getNodeState(ip) {
         body: JSON.stringify(requestData),
     })
         .then(response => response.json())
-        .then(data => console.log(JSON.stringify(data)))
+        .then(data => {
+            console.log(data.result);
+        })
         .catch(error => console.error(error));
 }
 
@@ -79,7 +127,9 @@ function getVersion(ip) {
         body: JSON.stringify(requestData),
     })
         .then(response => response.json())
-        .then(data => console.log(JSON.stringify(data)))
+        .then(data => {
+            console.log(data.result);
+        })
         .catch(error => console.error(error));
 }
 
