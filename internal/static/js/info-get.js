be6685599f3846243fa3e2f3cab6588a0f4d7fbc
PELENIUM <i.d.chernyshov@yandex.ru>
2023-06-28 19:33:31 +0300

    .

--- internal/static/js/info-get.js
+++ internal/static/js/info-get.js
@@ -13,7 +13,7 @@ async function main() {
         }
 
         for (var i = 0; i < data.length; i++) {
-            console.log(data[i][0], data[i][1], data[i][2])
+            console.log(data)
             // var ip = data[i].trim();
 
             // console.log(ip);
