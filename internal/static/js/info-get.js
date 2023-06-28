6b73a6c4551ef52824cf3ae220f681d4e1d150fd
PELENIUM <i.d.chernyshov@yandex.ru>
2023-06-28 19:44:49 +0300

    .

--- internal/static/js/info-get.js
+++ internal/static/js/info-get.js
@@ -14,17 +14,17 @@ async function main() {
 
         for (var i = 0; i < data.length; i++) {
             console.log(data[i].ip, data[i].blocks_ever, data[i].blocks_today);
-            // var ip = data[i].trim();
+            var ip = data[i].ip.trim();
 
-            // console.log(ip);
+            console.log(ip);
 
-            // const blockHeight = await getBlockHeight(ip);
-            // const blockCount = data[i].blocks_ever;
-            // console.log(`height - ${blockHeight}\ncount - ${blockCount}`);
-            // const nodeState = data[i].blocks_ever;
-            // const version = await getVersion(ip);
+            const blockHeight = await getBlockHeight(ip);
+            const blockNumberEver = data[i].blocks_ever;
+            const blockNumberToday = data[i].blocks_today;
+            const nodeState = await getNodeState(ip);
+            const version = await getVersion(ip);
 
-            // createCard(ip, blockHeight, version, blockCount, blockCount, nodeState);
+            createCard(ip, blockHeight, version, blockNumberEver, blockNumberToday, nodeState);
         }
     } catch (error) {
         console.error(error);
@@ -53,30 +53,30 @@ function getBlockHeight(ip) {
 async function getBlockCount(ip) {
     const url = `http://${ip}:30003`;
     const requestData = {
-      jsonrpc: '2.0',
-      method: 'getblockcount',
-      params: {},
-      id: 1,
+        jsonrpc: '2.0',
+        method: 'getblockcount',
+        params: {},
+        id: 1,
     };
-  
+
     try {
-      const response = await fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(requestData),
-      });
-      const data = await response.json();
-  
-      if (response.ok) {
-        return data.result; // Возвращаем общее количество блоков
-      } else {
-        throw new Error(data.error.message);
-      }
+        const response = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(requestData),
+        });
+        const data = await response.json();
+
+        if (response.ok) {
+            return data.result; // Возвращаем общее количество блоков
+        } else {
+            throw new Error(data.error.message);
+        }
     } catch (error) {
-      console.error('Произошла ошибка:', error.message);
-      // Обработка ошибки - можно вернуть значение по умолчанию или выбрать другой способ обработки.
-      return null;
+        console.error('Произошла ошибка:', error.message);
+        // Обработка ошибки - можно вернуть значение по умолчанию или выбрать другой способ обработки.
+        return null;
     }
-  }
+}
 
 function getNodeState(ip) {
     const url = `http://${ip}:30003`;
@@ -116,7 +116,7 @@ function getVersion(ip) {
         .catch(error => console.error(error));
 }
 
-function createCard(ip, blockHeight, version, minedToday, minedForAllTime, nodeState) {
+function createCard(ip, blockHeight, version, minedForAllTime, minedToday, nodeState) {
     const card = document.createElement('div');
     card.className = 'node-card';
 
