f4b908259a2cc5b84e640c0a6fdaeebb84e34989
PELENIUM <i.d.chernyshov@yandex.ru>
2023-06-28 19:21:03 +0300

    fixed a bug

--- internal/handlers/api.go
+++ internal/handlers/api.go
@@ -18,9 +18,10 @@ func Api(db *sql.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		result := []data{}
 
-		rows, err := db.Query("SELECT ip, blocks_ever, blocks_today FROM nodes_ip")
+		rows, err := db.Query("SELECT ip, blocks_ever, blocks_today FROM nodes_ip;")
 		if err != nil {
 			panic(err)
+			fmt.Println("mistake")
 		}
 		defer rows.Close()
 
