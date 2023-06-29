4fe1f30b64bc6d3b002971ccda108ae2f7fdc5d0
PELENIUM <i.d.chernyshov@yandex.ru>
2023-06-29 12:48:42 +0300

    .

--- internal/static/js/info-get.js
+++ internal/static/js/info-get.js
@@ -61,11 +61,11 @@ function sendData(jsn, ever, today) {
             console.log("it's ok");
         }
     };
-    // ip: jsn,
-    // blocks_ever: ever,
-    // blocks_today: today,
+    
     var data = JSON.stringify({
-        some_key: "aaaaaaaa",
+        ip: jsn,
+        blocks_ever: ever,
+        blocks_today: today,
     });
     xhr.send(data);
 }
