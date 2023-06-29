fb859c88df814b06b4e68cf8c66e030026f602ea
PELENIUM <i.d.chernyshov@yandex.ru>
2023-06-29 23:25:40 +0300

    .

--- internal/handlers/mainPage.go
+++ internal/handlers/mainPage.go
@@ -22,69 +22,64 @@ func NodeIpPOST(db *sql.DB) gin.HandlerFunc {
 		if err != nil {
 			panic(err)
 		}
-		arr := gjson.Get(string(jsn), "ip").Array()
-		
-		fmt.Println(len(arr))
-		for i := 0; i < len(arr); i++ {
-			ip_first := gjson.Get(string(jsn), fmt.Sprintf("ip.%d", i)).String()
-	
-			var ip string
-			if ip_first[len(ip_first)-1:] == " " {
-				ip = ip_first[:len(ip_first)-1]
-			} else {
-				ip = ip_first
+
+		ip_first := gjson.Get(string(jsn), "ip").String()
+		var ip string
+		if ip_first[len(ip_first)-1:] == " " {
+			ip = ip_first[:len(ip_first)-1]
+		} else {
+			ip = ip_first
+		}
+
+		fmt.Println(ip)
+
+		if ip != "" {
+			add := "INSERT INTO nodes_ip (ip) VALUES(?)"
+
+			var notExists bool
+
+			err := db.QueryRow(`SELECT EXISTS(SELECT 1 FROM nodes_ip WHERE ip = ?)`, ip).Scan(&notExists)
+
+			if err != nil {
+				panic(err)
 			}
-	
-			fmt.Println(ip)
-	
-			if ip != "" {
-				add := "INSERT INTO nodes_ip (ip) VALUES(?)"
-	
-				var notExists bool
-	
-				err := db.QueryRow(`SELECT EXISTS(SELECT 1 FROM nodes_ip WHERE ip = ?)`, ip).Scan(&notExists)
-	
+
+			if !notExists {
+				fmt.Println("there no node with such ip")
+				_, err = db.Exec(add, ip)
+
 				if err != nil {
 					panic(err)
 				}
-	
-				if !notExists {
-					fmt.Println("there no node with such ip")
-					_, err = db.Exec(add, ip)
-	
-					if err != nil {
-						panic(err)
-					}
-				} else {
-					fmt.Println("there's such ip")
-				}
-	
-				rows, err := db.Query("SELECT * FROM nodes_ip")
+			} else {
+				fmt.Println("there's such ip")
+			}
+
+			rows, err := db.Query("SELECT * FROM nodes_ip")
+			if err != nil {
+				panic(err)
+			}
+
+			defer rows.Close()
+			cols, err := rows.Columns()
+			if err != nil {
+				panic(err)
+			}
+
+			all_ips := make([]interface{}, len(cols))
+			for i := range cols {
+				all_ips[i] = new(interface{})
+			}
+
+			for rows.Next() {
+				err = rows.Scan(all_ips...)
 				if err != nil {
 					panic(err)
 				}
-	
-				defer rows.Close()
-				cols, err := rows.Columns()
-				if err != nil {
-					panic(err)
-				}
-	
-				all_ips := make([]interface{}, len(cols))
-				for i := range cols {
-					all_ips[i] = new(interface{})
-				}
-	
-				for rows.Next() {
-					err = rows.Scan(all_ips...)
-					if err != nil {
-						panic(err)
-					}
-	
-					for i, column := range cols {
-						val := *(all_ips[i].(*interface{}))
-						fmt.Println(column, val)
-					}
+
+				for i, column := range cols {
+					val := *(all_ips[i].(*interface{}))
+					fmt.Println(column, val)
 				}
 			}
 		}
