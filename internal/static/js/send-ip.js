f81252dd69ffdb69296016617bba7e88bf470629
PELENIUM <i.d.chernyshov@yandex.ru>
2023-06-29 21:23:59 +0300

    .

--- internal/static/js/send-ip.js
+++ internal/static/js/send-ip.js
@@ -4,7 +4,7 @@ document.getElementById("submit").addEventListener("click", async function () {
 });
 
 
-async function sendIP() {
+function sendIP() {
     var ip = String(document.getElementById("ip").value);
     var url = "/";
     var arr = ip.split(" ");
@@ -12,18 +12,11 @@ async function sendIP() {
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-Type", "application/json");
 
-    var i = 0;
-
-    function sendNextIP() {
-        if (i < arr.length) {
-            var data = JSON.stringify({
-                ip: arr[i],
-            });
-            xhr.send(data);
-            i++;
-
-            setTimeout(sendNextIP, 100);
-        }
+    for (var i = 0; i < arr.length; i++) {
+        var data = JSON.stringify({
+            ip: arr[i],
+        });
+        xhr.send(data);
     }
 
     sendNextIP();
