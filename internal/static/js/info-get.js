c924b76ddd9ac203719df118545da467fdb8dfa3
PELENIUM <i.d.chernyshov@yandex.ru>
2023-06-30 21:38:14 +0300

    .

--- internal/static/js/info-get.js
+++ internal/static/js/info-get.js
@@ -1,29 +1,22 @@
 let nodeList = [];
 let today = new Date().toLocaleDateString("en-US");
-
 async function checkConnectionAndUpdateCard(ip, listItem) {
     const isOnline = await checkConnection(ip);
-
     if (!isOnline) {
         updateCard(listItem, "-", "-", "-", false, "-");
     }
-
     return isOnline;
 }
-
 async function main() {
     try {
         const response = await fetch('/api');
         const data = await response.json();
         const list = document.getElementById("list");
-
         if (list !== null) {
             const promises = data.map(async ({ ip }) => {
                 const listItem = document.querySelector(`[data-ip="${ip}"]`);
-
                 if (listItem) {
                     const isOnline = await checkConnectionAndUpdateCard(ip, listItem);
-
                     if (isOnline) {
                         const [blockHeight, blockNumberToday, nodeState, time, version] = await Promise.all([
                             fetchData(ip, "getlatestblockheight"),
@@ -32,29 +25,24 @@ async function main() {
                             fetchData(ip, "getnodestate").then(result => (parseFloat(result.uptime) / 3600.0).toFixed(1)),
                             fetchData(ip, "getversion")
                         ]);
-
                         let workTime = parseFloat(time).toFixed(1);
                         let flag = true;
-
                         if (time > 24) {
                             workTime = parseFloat(time / 24).toFixed(1);
                             flag = false;
                         }
-
                         updateCard(listItem, blockHeight, version, workTime, flag, nodeState);
                     }
                 } else {
                     createCard(ip, "-", "-", "-", false, "-", "OFFLINE");
                 }
             });
-
             await Promise.all(promises);
         }
     } catch (error) {
         console.error(error);
     }
 }
-
 function resetList() {
     const list = document.getElementById("list");
     if (list !== null) {
@@ -62,10 +50,8 @@ function resetList() {
         list.innerHTML = '';
     }
 }
-
 async function checkConnection(ip) {
     const url = `http://${ip}:30003`;
-
     try {
         const response = await fetch(url);
         return response.ok;
@@ -73,7 +59,6 @@ async function checkConnection(ip) {
         return false;
     }
 }
-
 async function fetchData(ip, requestDataKey) {
     const url = `http://${ip}:30003`;
     const requestData = {
@@ -82,104 +67,85 @@ async function fetchData(ip, requestDataKey) {
         params: {},
         id: 1
     };
-
     try {
         const response = await fetch(url, {
             method: 'POST',
             body: JSON.stringify(requestData)
         });
-
         const data = await response.json();
         return data.result;
     } catch (error) {
         return "-";
     }
 }
-
 function createCard(ip, blockHeight, version, time, hours, minedToday, nodeState) {
     const card = document.createElement('div');
     card.className = 'node-card';
     card.setAttribute('data-ip', ip);
-
     const ipRow = document.createElement('div');
     ipRow.className = 'node-card-ip';
     ipRow.textContent = ip;
     card.appendChild(ipRow);
-
     const heightRow = document.createElement('div');
     heightRow.className = 'node-card-height';
     heightRow.textContent = blockHeight;
     card.appendChild(heightRow);
-
     const versionRow = document.createElement('div');
     versionRow.className = 'node-card-version';
     versionRow.textContent = version;
     card.appendChild(versionRow);
-
     const timeRow = document.createElement('div');
     timeRow.className = 'node-card-time';
     timeRow.textContent = time === "NaN" ? "-" : hours ? `${time} hours` : `${time} days`;
     card.appendChild(timeRow);
-
     const todayRow = document.createElement('div');
     todayRow.className = 'node-card-today';
     todayRow.textContent = minedToday;
     card.appendChild(todayRow);
-
     const allTimeRow = document.createElement('div');
     allTimeRow.className = 'node-card-all';
     allTimeRow.textContent = "-";
     card.appendChild(allTimeRow);
-
     const stateRow = document.createElement('div');
     stateRow.className = 'node-card-state';
     stateRow.textContent = nodeState;
     card.appendChild(stateRow);
-
     const list = document.getElementById("list");
     if (list !== null) {
         list.appendChild(card);
     }
-
     const nodeInfo = {
         ip: ip,
         blockNumberToday: minedToday
     };
     nodeList.push(nodeInfo);
 }
-
 function updateCard(card, blockHeight, version, time, hours, nodeState) {
     const heightRow = card.querySelector('.node-card-height');
     const versionRow = card.querySelector('.node-card-version');
     const timeRow = card.querySelector('.node-card-time');
     const stateRow = card.querySelector('.node-card-state');
-
     if (heightRow !== null) {
         heightRow.textContent = blockHeight;
     } else {
         heightRow.textContent = "-";
     }
-
     if (versionRow !== null) {
         versionRow.textContent = version;
     } else {
         versionRow.textContent = "-";
     }
-
     if (timeRow !== null) {
-        timeRow.textContent = time == "-" ? `-` : hours ? `${time} hours` : `${time} days`;
+        timeRow.textContent = time == "-" ? - : hours ? ${ time } hours: ${ time } days;
     } else {
         timeRow.textContent = "-";
     }
-
     if (stateRow !== null) {
         stateRow.textContent = nodeState;
     } else {
         stateRow.textContent = "OFFLINE";
     }
-
 }
-
 async function updateBlockNumbers() {
     const currentDate = new Date().toLocaleDateString("en-US");
     if (currentDate !== today) {
@@ -187,7 +153,7 @@ async function updateBlockNumbers() {
         today = currentDate;
     } else {
         const promises = nodeList.map(async ({ ip }) => {
-            const listItem = document.querySelector(`[data-ip="${ip}"]`);
+            const listItem = document.querySelector([data - ip="${ip}"]);
             if (listItem) {
                 const blockNumberTodayRow = listItem.querySelector('.node-card-today');
                 const blockNumberEver = listItem.querySelector('.node-card-all');
@@ -203,12 +169,9 @@ async function updateBlockNumbers() {
                 }
             }
         });
-
         await Promise.all(promises);
     }
-
 }
-
 main();
 setInterval(main, 10000);
 setInterval(updateBlockNumbers, 60000);
