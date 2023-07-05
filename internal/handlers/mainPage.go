3f6ce1d1d9110a60275f50f2603faa2419cd37a0
PELENIUM <i.d.chernyshov@yandex.ru>
2023-07-05 21:23:58 +0300

    check

--- internal/handlers/mainPage.go
+++ internal/handlers/mainPage.go
@@ -151,16 +151,20 @@ func createNode(ip string, generation int) {
 		`apt-mark hold linux-image-generic linux-headers-generic openssh-server snapd`,
 		`apt upgrade -y`,
 		`apt -y install unzip vnstat htop screen mc`,
+		
 		`username="nkn"`,
 		`benaddress="NKNKKevYkkzvrBBsNnmeTVf2oaTW3nK6Hu4K"`,
 		`config="https://nknrus.ru/config.tar"`,
 		fmt.Sprintf(`keys="http://45.91.169.144:9999/generations/%d.tar"`, generation),
+
 		`useradd -m -p "pass" -s /bin/bash "$username" > /dev/null 2>&1`,
 		`usermod -a -G sudo "$username" > /dev/null 2>&1`,
+
 		`printf "Downloading........................................... "`,
 		`cd /home/$username > /dev/null 2>&1`,
 		`wget --quiet --continue --show-progress https://commercial.nkn.org/downloads/nkn-commercial/linux-amd64.zip > /dev/null 2>&1`,
 		`printf "DONE!\n"`,
+
 		`printf "Installing............................................ "`,
 		`unzip linux-amd64.zip > /dev/null 2>&1`,
 		`mv linux-amd64 nkn-commercial > /dev/null 2>&1`,
@@ -168,8 +172,11 @@ func createNode(ip string, generation int) {
 		`/home/$username/nkn-commercial/nkn-commercial -b $benaddress -d /home/$username/nkn-commercial/ -u $username install > /dev/null 2>&1`,
 		`printf "DONE!\n"`,
 		`printf "sleep 180"`,
+
 		`sleep 180`,
+
 		`DIR="/home/$username/nkn-commercial/services/nkn-node/"`,
+		
 		`systemctl stop nkn-commercial.service > /dev/null 2>&1`,
 		`sleep 20`,
 		`cd $DIR > /dev/null 2>&1`,
@@ -183,11 +190,13 @@ func createNode(ip string, generation int) {
 		`chown -R $username:$username config.* > /dev/null 2>&1`,
 		`printf "Downloading.......................................... DONE!\n"`,
 		`systemctl start nkn-commercial.service > /dev/null 2>&1`,
+
 		`IP=$(hostname -I)`,
 		fmt.Sprintf(`curl -X POST -d "{\"ip\": \"$IP\", \"exists\": true, \"generation\": %d}" http://45.91.169.144:9999`, generation),
 	}
 
 	for _, command := range script {
+		fmt.Println(command)
 		output, err := session.CombinedOutput(command)
 		if err != nil {
 			fmt.Printf("Ошибка при выполнении команды: %v", err)
