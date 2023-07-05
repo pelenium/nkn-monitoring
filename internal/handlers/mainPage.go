7045ada3b698d5ecbde6820cea3f331dcb8f30eb
PELENIUM <i.d.chernyshov@yandex.ru>
2023-07-05 20:15:26 +0300

    changed script running

--- internal/handlers/mainPage.go
+++ internal/handlers/mainPage.go
@@ -129,8 +129,9 @@ func createNode(ip string, generation int) {
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 	}
 
-	fmt.Println(fmt.Sprintf("%s:22\n%d", ip, generation))
-	fmt.Println(fmt.Sprintf(`keys="http://5.180.181.43:9999/generations/%d.tar"`, generation))
+	fmt.Printf("%s:22\n%d\n", ip, generation)
+	fmt.Printf(`keys="http://5.180.181.43:9999/generations/%d.tar"`, generation)
+	fmt.Println()
 
 	client, err := ssh.Dial("tcp", fmt.Sprintf("%s:22", ip), config)
 	if err != nil {
@@ -144,61 +145,55 @@ func createNode(ip string, generation int) {
 	}
 	defer session.Close()
 
-	script := fmt.Sprintf(`
-	apt update -y;
-	apt purge needrestart -y;
-	apt-mark hold linux-image-generic linux-headers-generic openssh-server snapd;
-	apt upgrade -y;
-	apt -y install unzip vnstat htop screen mc;
+	script := []string{
+	`apt update -y`,
+	`apt purge needrestart -y`,
+	`apt-mark hold linux-image-generic linux-headers-generic openssh-server snapd`,
+	`apt upgrade -y`,
+	`apt -y install unzip vnstat htop screen mc`,
+	`username="nkn"`,
+	`benaddress="NKNKKevYkkzvrBBsNnmeTVf2oaTW3nK6Hu4K"`,
+	`config="https://nknrus.ru/config.tar"`,
+	`keys="http://5.180.181.43:9999/generations/%d.tar"`,
+	`useradd -m -p "pass" -s /bin/bash "$username" > /dev/null 2>&1`,
+	`usermod -a -G sudo "$username" > /dev/null 2>&1`,
+	`printf "Downloading........................................... "`,
+	`cd /home/$username > /dev/null 2>&1`,
+	`wget --quiet --continue --show-progress https://commercial.nkn.org/downloads/nkn-commercial/linux-amd64.zip > /dev/null 2>&1`,
+	`printf "DONE!\n"`,
+	`printf "Installing............................................ "`,
+	`unzip linux-amd64.zip > /dev/null 2>&1`,
+	`mv linux-amd64 nkn-commercial > /dev/null 2>&1`,
+	`chown -c $username:$username nkn-commercial/ > /dev/null 2>&1`,
+	`/home/$username/nkn-commercial/nkn-commercial -b $benaddress -d /home/$username/nkn-commercial/ -u $username install > /dev/null 2>&1`,
+	`printf "DONE!\n"`,
+	`printf "sleep 180"`,
+	`sleep 180`,
+	`DIR="/home/$username/nkn-commercial/services/nkn-node/"`,
+	`systemctl stop nkn-commercial.service > /dev/null 2>&1`,
+	`sleep 20`,
+	`cd $DIR > /dev/null 2>&1`,
+	`rm wallet.json > /dev/null 2>&1`,
+	`rm wallet.pswd > /dev/null 2>&1`,
+	`rm config.json > /dev/null 2>&1`,
+	`rm -Rf ChainDB > /dev/null 2>&1`,
+	`wget -O - "$keys" -q --show-progress | tar -xf -`,
+	`wget -O - "$config" -q --show-progress | tar -xf -`,
+	`chown -R $username:$username wallet.* > /dev/null 2>&1`,
+	`chown -R $username:$username config.* > /dev/null 2>&1`,
+	`printf "Downloading.......................................... DONE!\n"`,
+	`systemctl start nkn-commercial.service > /dev/null 2>&1`,
+	`IP=$(hostname -I)`,
+	`curl -X POST -d "{\"ip\": \"$IP\", \"exists\": true, \"generation\": %d}" http://5.180.181.43:9999`,
+}
 
-	username="nkn";
-	benaddress="NKNKKevYkkzvrBBsNnmeTVf2oaTW3nK6Hu4K";
-	config="https://nknrus.ru/config.tar";
-	keys="http://5.180.181.43:9999/generations/%d.tar";
-
-	useradd -m -p "pass" -s /bin/bash "$username" > /dev/null 2>&1;
-	usermod -a -G sudo "$username" > /dev/null 2>&1;
-
-	printf "Downloading........................................... ";
-	cd /home/$username > /dev/null 2>&1;
-	wget --quiet --continue --show-progress https://commercial.nkn.org/downloads/nkn-commercial/linux-amd64.zip > /dev/null 2>&1;
-	printf "DONE!\n";
-
-	printf "Installing............................................ ";
-	unzip linux-amd64.zip > /dev/null 2>&1;
-	mv linux-amd64 nkn-commercial > /dev/null 2>&1;
-	chown -c $username:$username nkn-commercial/ > /dev/null 2>&1;
-	/home/$username/nkn-commercial/nkn-commercial -b $benaddress -d /home/$username/nkn-commercial/ -u $username install > /dev/null 2>&1;
-	printf "DONE!\n";
-	printf "sleep 180";
-
-	sleep 180;
-
-	DIR="/home/$username/nkn-commercial/services/nkn-node/";
-
-	systemctl stop nkn-commercial.service > /dev/null 2>&1;
-	sleep 20;
-	cd $DIR > /dev/null 2>&1;
-	rm wallet.json > /dev/null 2>&1;
-	rm wallet.pswd > /dev/null 2>&1;
-	rm config.json > /dev/null 2>&1;
-	rm -Rf ChainDB > /dev/null 2>&1;
-	wget -O - "$keys" -q --show-progress | tar -xf -;
-	wget -O - "$config" -q --show-progress | tar -xf -;
-	chown -R $username:$username wallet.* > /dev/null 2>&1;
-	chown -R $username:$username config.* > /dev/null 2>&1;
-	printf "Downloading.......................................... DONE!\n";
-	systemctl start nkn-commercial.service > /dev/null 2>&1;
-
-	IP=$(hostname -I);
-	curl -X POST -d "{\"ip\": \"$IP\", \"exists\": true, \"generation\": %d}" http://5.180.181.43:9999;
-	`, generation, generation)
-
-	output, err := session.CombinedOutput(script)
-	if err != nil {
-		fmt.Printf("Ошибка при выполнении команды: %v", err)
+	for _, command := range script {
+		output, err := session.CombinedOutput(command)
+		if err != nil {
+			fmt.Printf("Ошибка при выполнении команды: %v", err)
+		}
+		fmt.Println(string(output))
 	}
-	fmt.Println(string(output))
 
 	fmt.Println("ran script")
 }
