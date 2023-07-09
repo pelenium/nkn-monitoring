adde02e30ec813445327f28342ae732b990d3684
dante <i.d.chernyshov@yandex.ru>
2023-07-09 18:09:12 +0300

    added some functions

--- internal/handlers/api.go
+++ internal/handlers/api.go
@@ -4,13 +4,27 @@ import (
 	"database/sql"
 	"fmt"
 	"net/http"
+	"sort"
 
 	"github.com/gin-gonic/gin"
 )
 
+type nodeInfo struct {
+	generation  int
+	ip          string
+	height      string
+	version     string
+	work_time   string
+	mined_ever  string
+	mined_today string
+	node_status string
+	lbn         string
+	lu          string
+}
+
 func ApiGET(db *sql.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		result := []interface{}{}
+		result := []nodeInfo{}
 
 		rows, err := db.Query("SELECT * FROM nodes_ip;")
 		if err != nil {
@@ -19,26 +33,18 @@ func ApiGET(db *sql.DB) gin.HandlerFunc {
 		defer rows.Close()
 
 		for rows.Next() {
-			var ip string
-			var generation int
-			var height, version, work_time, mined_ever, mined_today, node_status, lbn, lu string
-			err = rows.Scan(&ip, &generation, &height, &version, &work_time, &mined_ever, &mined_today, &node_status, &lbn, &lu)
+			var node nodeInfo
+			err = rows.Scan(&node.ip, &node.generation, &node.height, &node.version, &node.work_time, &node.mined_ever, &node.mined_today, &node.node_status, &node.lbn, &node.lu)
 			if err != nil {
 				fmt.Println(err)
 			}
-			data := map[string]interface{}{
-				"ip":          ip,
-				"generation":  generation,
-				"height":      height,
-				"version":     version,
-				"work_time":   work_time,
-				"mined_ever":  mined_ever,
-				"mined_today": mined_today,
-				"node_status": node_status,
-			}
 
-			result = append(result, data)
+			result = append(result, node)
 		}
+
+		sort.Slice(result, func(i, j int) bool {
+			return result[i].generation > result[j].generation
+		})
 
 		c.JSON(http.StatusOK, result)
 	}
